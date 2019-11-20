df652c53f52d167fc154731c4cf141862a2b81b5
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2019-11-20 23:58:36 +0200

    modified stopwatch logic

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,4 +1,37 @@
-const sw = new Stopwatch();
+// const sw = new Stopwatch();
+
+// function Stopwatch() {
+//   let startTimer,
+//     stopTimer,
+//     running,
+//     duration = 0;
+
+//   this.start = function() {
+//     if (running) throw new Error("Stopwatch already started");
+//     running = true;
+//     startTimer = new Date().getTime();
+//   };
+//   this.stop = function() {
+//     if (!running) throw new Error("Stopwatch is not started");
+
+//     running = false;
+//     stopTimer = new Date().getTime();
+
+//     let seconds = (stopTimer - startTimer) / 1000;
+//     duration += seconds;
+//   };
+//   this.reset = function() {
+//     startTimer;
+//     stopTimer;
+//     running;
+//     duration = 0;
+//   };
+//   Object.defineProperty(this, "duration", {
+//     get: () => duration
+//   });
+// }
+
+let sw = new Stopwatch();
 
 function Stopwatch() {
   let startTimer,
@@ -7,26 +40,21 @@ function Stopwatch() {
     duration = 0;
 
   this.start = function() {
-    if (running) throw new Error("Stopwatch already started");
-    running = true;
+    if (running) throw new Error("Stopwatch has already been started");
+
+    running = false;
     startTimer = new Date().getTime();
   };
   this.stop = function() {
     if (!running) throw new Error("Stopwatch is not started");
 
-    running = false;
+    running = true;
     stopTimer = new Date().getTime();
 
     let seconds = (stopTimer - startTimer) / 1000;
     duration += seconds;
   };
   this.reset = function() {
-    startTimer;
-    stopTimer;
-    running;
-    duration = 0;
+    startTimer, stopTimer, running, (duration = 0);
   };
-  Object.defineProperty(this, "duration", {
-    get: () => duration
-  });
 }
