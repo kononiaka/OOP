9c7716bca88f80c011378f0776b75521ffc8b158
kononiaka <kononiaka.vladimir@gmail.com>
2019-11-26 16:46:49 +0200

    refactored code for stopwatch()

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,36 +1,3 @@
-// const sw = new Stopwatch();
-
-// function Stopwatch() {
-//   let startTimer,
-//     stopTimer,
-//     running,
-//     duration = 0;
-
-//   this.start = function() {
-//     if (running) throw new Error("Stopwatch already started");
-//     running = true;
-//     startTimer = new Date().getTime();
-//   };
-//   this.stop = function() {
-//     if (!running) throw new Error("Stopwatch is not started");
-
-//     running = false;
-//     stopTimer = new Date().getTime();
-
-//     let seconds = (stopTimer - startTimer) / 1000;
-//     duration += seconds;
-//   };
-//   this.reset = function() {
-//     startTimer;
-//     stopTimer;
-//     running;
-//     duration = 0;
-//   };
-//   Object.defineProperty(this, "duration", {
-//     get: () => duration
-//   });
-// }
-
 let sw = new Stopwatch();
 
 function Stopwatch() {
@@ -38,23 +5,27 @@ function Stopwatch() {
     stopTimer,
     running,
     duration = 0;
-
   this.start = function() {
-    if (running) throw new Error("Stopwatch has already been started");
-
-    running = false;
-    startTimer = new Date().getTime();
-  };
-  this.stop = function() {
-    if (!running) throw new Error("Stopwatch is not started");
-
+    if (running) {
+      throw new Error("Stopwatch already started");
+    }
     running = true;
-    stopTimer = new Date().getTime();
+    startTimer = new Date().getTime();
+    this.stop = function() {
+      if (!running) {
+        throw new Error(`Stopwatch didn't start`);
+      }
+      running = false;
+      stopTimer = new Date().getTime();
 
-    let seconds = (stopTimer - startTimer) / 1000;
-    duration += seconds;
-  };
-  this.reset = function() {
-    startTimer, stopTimer, running, (duration = 0);
+      let seconds = (stopTimer - startTimer) / 1000;
+      duration += seconds;
+    };
+    this.reset = function() {
+      startTimer, stopTimer, running, (duration = 0);
+    };
+    Object.defineProperty(this, "duration", {
+      get: () => duration
+    });
   };
 }
