ac2b57ce70e016464478b10a8058f8456dc308b7
kononiaka <kononiaka.vladimir@gmail.com>
2019-11-29 18:19:02 +0200

    Prototypical inheritance lessons were added

--- js/inheritance.js
+++ js/inheritance.js
@@ -5,3 +5,28 @@ Prototype is a parent of another object
 /* LESSON 3
 Objects created by given constructor will have the same prototype
  */
+
+/*LESSON 4
+let person = { name: "Vlad" };
+
+let objectBase = Object.getPrototypeOf(person);
+let descriptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
+
+Object.defineProperty(person, "name", {
+  writable: false, //couldn't change the value of attribute
+  enurable: false, //won't show in a console
+  configurable: false //can't delete property
+});
+
+console.log(person);
+*/
+
+/*LESSON 5
+let array = [];
+
+Object.getPrototypeOf(array);  //the same as array.__proto__
+
+let arr = new Array();
+Array.prototype;
+arr.constructor.prototype;
+*/
